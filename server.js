51eaf73d3dbce2dc962c97cb3e548fb6bce5b92c
aakash.bathla1 <aakash.bathla@jaarvistech.com>
2018-02-23 02:56:21 +0530

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 hbs.registerHelper('currentYear',()=>{
@@ -20,6 +21,6 @@ app.get('/about',(req,res)=>{
 		pageTitle: 'About Page'		
 	})
 })
-app.listen(3000,()=>{
-	console.log('server is up on port 3000');
+app.listen(port,()=>{
+	console.log(`server is up on port ${port}`);
 });
